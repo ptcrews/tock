b501dda7d919884ccf946763565590812c3a8f27
Mateo Garcia <mateog@stanford.edu>
2017-07-04 14:22:14 -0700

    Ready to implement SLIP (serial line IP) from radio_rx on imix to rpi.

---
+++ userland/examples/radio_rx_slip/main.c
@@ -0,0 +1,39 @@
+#include <stdbool.h>
+#include <stdio.h>
+
+#include <led.h>
+#include <radio.h>
+#include <timer.h>
+
+#define BUF_SIZE 60
+char packet_rx[BUF_SIZE];
+char packet_tx[BUF_SIZE];
+bool toggle = true;
+
+static void callback(__attribute__ ((unused)) int unused0,
+                     __attribute__ ((unused)) int unused1,
+                     __attribute__ ((unused)) int unused2,
+                     __attribute__ ((unused)) void* ud) {
+  led_toggle(0);
+
+  printf("%s\n", packet_rx);  // TODO: SEND OVER SLIP
+
+  radio_receive_callback(callback, packet_rx, BUF_SIZE);
+}
+
+int main(void) {
+  int i;
+
+  printf("START 802.15.4 RECEIVE\n");
+
+  for (i = 0; i < BUF_SIZE; i++) {
+    packet_rx[i] = packet_tx[i] = i;
+  }
+  radio_set_addr(0x0802);
+  radio_set_pan(0xABCD);
+  radio_commit();
+  radio_receive_callback(callback, packet_rx, BUF_SIZE);
+  while (1) {
+    delay_ms(4000);
+  }
+}
