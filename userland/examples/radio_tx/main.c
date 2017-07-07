5a51c41efb1edd71f126ddb1f85041002f258be0
Mateo Garcia <mateog@stanford.edu>
2017-07-07 16:40:59 -0700

    Update radio tx to version that sends 60 byte payload every 2s.

--- userland/examples/radio_tx/main.c
+++ userland/examples/radio_tx/main.c
@@ -1,4 +1,5 @@
 #include <stdbool.h>
+#include <stdio.h>
 
 #include "gpio.h"
 #include "led.h"
@@ -10,21 +11,34 @@
 char packet[BUF_SIZE];
 bool toggle = true;
 
+/* SLIP special character codes, written in octal */
+#define END             0300    /* indicates end of packet */
+#define ESC             0333    /* indicates byte stuffing */
+#define ESC_END         0334    /* ESC ESC_END means END data byte */
+#define ESC_ESC         0335    /* ESC ESC_ESC means ESC data byte */
+
 int main(void) {
   int i;
   for (i = 0; i < BUF_SIZE; i++) {
-    packet[i] = i;
+    packet[i] = 'a';
   }
   gpio_enable_output(0);
   radio_init();
+
+  printf("START 802.15.4 TRANSMIT\n");
+
+  printf("END:     %x\n", END);
+  printf("ESC:     %x\n", ESC);
+  printf("ESC_END: %x\n", ESC_END);
+  printf("ESC_ESC: %x\n", ESC_ESC);
+
   radio_set_addr(0x1540);
-  radio_init();
   radio_set_pan(0xABCD);
-  radio_init();
+  radio_commit();             // START HERE
   while (1) {
     led_toggle(0);
     int err = radio_send(0x0802, packet, BUF_SIZE);
-    if (err != SUCCESS) {
+    if (err != TOCK_SUCCESS) {
       gpio_toggle(0);
     }
     delay_ms(250);
