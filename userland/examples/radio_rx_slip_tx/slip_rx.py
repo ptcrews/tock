60370320231068c20b2fc7956951f937336afa3d
Mateo Garcia <mateog@stanford.edu>
2017-07-07 17:40:40 -0700

    V weird...

--- userland/examples/radio_rx_slip_tx/slip_rx.py
+++ userland/examples/radio_rx_slip_tx/slip_rx.py
@@ -96,4 +96,4 @@ with serial.Serial(addr, baud) as ser, open(log_dir + '/' + fname, fmode) as f, 
             file.write(packet)        # write line of text to file
             file.flush()              # make sure it actually gets written out
 
-        wrpcap(packet_dir + "/pkt_" + str(datetime.now()) + ".cap", packet)
+        wrpcap(packet_dir + "/pkt_" + str(datetime.datetime.now()) + ".cap", packet)
