4c11c3674b460a76339429229010ad164a7db729
Mateo Garcia <mateog@stanford.edu>
2017-07-07 17:38:49 -0700

    Fix?

--- userland/examples/radio_rx_slip_tx/slip_rx.py
+++ userland/examples/radio_rx_slip_tx/slip_rx.py
@@ -5,7 +5,7 @@
 import datetime
 import os
 import serial
-import scapy
+from scapy.all import *
 import sys
 
 addr      = '/dev/ttyUSB0'                   # serial port to read data from
@@ -96,4 +96,4 @@ with serial.Serial(addr, baud) as ser, open(log_dir + '/' + fname, fmode) as f, 
             file.write(packet)        # write line of text to file
             file.flush()              # make sure it actually gets written out
 
-        wrpcap(packet_dir + "/pkt_" + str(datetime.datetime.now()) + ".cap", [packet])
+        wrpcap(packet_dir + "/pkt_" + str(datetime.datetime.now()) + ".cap", packet)
