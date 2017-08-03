4abce6f0763b37c21e8db8268f3e0fb8371ea1f1
Mateo Garcia <mateog@stanford.edu>
2017-08-03 10:08:52 -0700

    Bailing on TLV formats. Going to hardcode MLE messages for now.

--- capsules/src/tlv.rs
+++ capsules/src/tlv.rs
@@ -1,5 +1,6 @@
 //! Implements Thread Type-Length-Value (TLV) formats
 
+/*
 pub enum Tlv {
     SourceAddress(u16),   // Sender's 16-bit MAC address
     Mode(/* TODO */),
@@ -10,12 +11,14 @@ pub enum Tlv {
     LinkQuality,                  // Not used in Thread
     NetworkParameter,             // Not used in Thread
     MleFrameCounter(u32),
-    Route64/* TODO (params) */,          // NOTE: Not required to implement SED
+    Route64 /* TODO: (params) */,          // NOTE: Not required to implement SED
     Address16(u16),
     LeaderData { partitionId: u32, weighting: u8, dataVersion: u8,
                  stableDataVersion: u8, leaderRouterId: u8 },
-    NetworkData/* TODO (params) */,      // NOTE: Not required to implement SED
-    TLVRequest,           // TODO: constructor will generate random byte string, 4 to 8 bytes in length
+    NetworkData /* TODO: (params) */,      // NOTE: Not required to implement SED
+    TlvRequest,
+    ScanMask(/* TODO */),   // Same as mode, neds to take variable number of enum options
+    Connectivity { parentPriority: ParentPriority, linkQuality3: u8, linkQuality2: u8, linkQuality1: u8 }
 }
 
 pub enum LinkMode {
@@ -27,3 +30,17 @@ pub enum LinkMode {
 
 // TODO: Constructor for link mode that takes variable length array of link mode options
 
+// Used when creating a Scan Mask TLV
+pub enum MulticastResponders {
+  Router,
+  EndDevice,
+}
+
+// Used in Connectivity TLV
+pub enum ParentPriority {
+  High = 0b01,
+  Medium = 0b00,
+  Low = 0b11,
+  // Reserved = 0b10
+}
+*/
