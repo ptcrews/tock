9c21bc26887ad121b632901692033efb843ecd31
hudson-ayers <hayers@stanford.edu>
2017-12-14 00:22:07 -0800

    A few more changes to ip.rs to remove some compile time warnings

--- capsules/src/net/ip.rs
+++ capsules/src/net/ip.rs
@@ -2,19 +2,10 @@
    of the networking stack. For a full description of the networking stack on
    tock, see the Thread_Stack_Design.txt document */
 
-use net::ip_utils::{IPAddr};
+use net::ip_utils::{IPAddr, IP6Header};
 use net::udp::{UDPPacket};
 use kernel::ReturnCode;
 
-
-pub struct IP6Header {
-    pub version_class_flow: [u8; 4],
-    pub payload_len: u16,
-    pub next_header: u8,
-    pub hop_limit: u8,
-    pub src_addr: IPAddr,
-    pub dst_addr: IPAddr,
-}
 
 pub enum TransportPacket<'a> { 
     UDP(UDPPacket<'a>),
@@ -29,25 +20,74 @@ pub struct IP6Packet<'a> {
     pub payload: TransportPacket<'a>,
 } 
 
-
 impl<'a> IP6Packet<'a> {
     pub fn reset(&self){} //Sets fields to appropriate defaults
     pub fn get_offset(&self) -> usize{40} //Always returns 40 until we add options support
     
     // Remaining functions are just getters and setters for the header fields
-    pub fn set_tf(&self, tf: u8){}
-    pub fn set_flow_label(&self, flow_label: u8){}
-    pub fn set_len(&self, len: u16){}
-    pub fn set_protocol(&self, proto: u8){}
-    pub fn set_dest_addr(&self, dest: IPAddr){}
-    pub fn set_src_addr(&self, src: IPAddr){}
-    pub fn get_tf(&self) -> u8{0}
-    pub fn get_flow_label(&self)-> u8{0}
-    pub fn get_len(&self) -> u16{0}
-    pub fn get_protocol(&self) -> u8{0}
+    pub fn set_traffic_class(&mut self, new_tc: u8){
+        self.header.set_traffic_class(new_tc);
+    }
+
+    pub fn set_dscp(&mut self, new_dscp: u8) {
+        self.header.set_dscp(new_dscp);
+    }
+
+    pub fn set_ecn(&mut self, new_ecn: u8) {
+        self.header.set_ecn(new_ecn);
+    }
+
+    pub fn set_flow_label(&mut self, flow_label: u32){
+        self.header.set_flow_label(flow_label);
+    }
+
+    pub fn set_payload_len(&mut self, len: u16){
+        self.header.set_payload_len(len);
+    }
+
+    pub fn set_next_header(&mut self, new_nh: u8){
+        self.header.set_next_header(new_nh);
+    }
+
+    pub fn set_hop_limit(&mut self, new_hl: u8) {
+        self.header.set_hop_limit(new_hl);
+    }
+
+    pub fn set_dest_addr(&mut self, dest: IPAddr){
+        self.header.dst_addr = dest;
+    }
+    pub fn set_src_addr(&mut self, src: IPAddr){
+        self.header.src_addr = src;
+    }
+
+    pub fn get_traffic_class(&self) -> u8{
+        self.header.get_traffic_class()
+    }
+
+    pub fn get_dscp(&self) -> u8{
+        self.header.get_dscp()
+    }
+
+    pub fn get_ecn(&self) -> u8{
+        self.header.get_ecn()
+    }
+
+    pub fn get_flow_label(&self)-> u32{
+        self.header.get_flow_label()
+    }
+
+    pub fn get_payload_len(&self) -> u16{
+        self.header.get_payload_len()
+    }
+
+    pub fn get_next_header(&self) -> u8{
+        self.header.get_next_header()
+    }
+
     pub fn get_dest_addr(&self) -> IPAddr{
         self.header.dst_addr
     }
+
     pub fn get_src_addr(&self) -> IPAddr{
         self.header.src_addr
     }
