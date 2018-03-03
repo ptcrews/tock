1eb3c8dc2e6c24115b4eee19b9cbafc75d8f4956
Conor McAvity <cmcavity@protonmail.com>
2018-03-02 21:34:44 -0800

    Add ICMP header struct and getter and setters

--- capsules/src/net/icmp.rs
+++ capsules/src/net/icmp.rs
@@ -1,30 +1,95 @@
+//! ICMP layer of the Tock networking stack.
+//!
+//! - Author: Conor McAvity <cmcavity@stanford.edu>
+
 use net::ip_utils::IPAddr;
 use kernel::ReturnCode;
+use net::stream::{decode_u16, decode_u8, decode_bytes};
+use net::stream::{encode_u16, encode_u8, encode_bytes};
+use net::stream::SResult;
 
 pub struct ICMPHeader {
-    pub type: u8,
     pub code: u8,
     pub cksum: u16,
+    pub options: ICMPHeaderOptions,
 }
 
-pub struct ICMPSocketExample {
-    pub src_ip: IPAddr,
+pub enum ICMPHeaderOptions {
+    Type0 { id: u16, seqno: u16 },
+    Type3 { _unused: u16, next_mtu: u16 },
 }
 
-pub trait ICMPSocket:ICMPSend {
-    fn bind(&self, src_ip: IPAddr) -> ReturnCode;
-    fn send(&self, dest: IPAddr, icmp_packet: &'static mut ICMPPacket) -> ReturnCode;
-    // TODO: What is this function used for??
-    fn send_done(&self, icmp_packet: &'static mut ICMPPacket, result: ReturnCode);
+impl Default for ICMPHeader {
+    fn default() -> ICMPHeader {
+        ICMPHeader {
+            code: 0,
+            cksum: 0,
+            options: Type0 { 0, 0 },
+        }
+    }
 }
 
-pub struct ICMPPacket<'a> {
-    pub header: ICMPHeader,
-    pub payload: &'a mut [u8],
-    pub len: u16,
+impl ICMPHeader {
+    pub fn new() -> ICMPHeader {
+        ICMPHeader::default()
+    }
+
+    pub fn set_type(&mut self, hdr_type: u8) {
+        match hdr_type {
+            0 => self.options = Type0 { 0, 0 },
+            3 => self.options = Type3 { 0, 0 }, 
+        };
+    }
+
+    pub fn set_code(&mut self, code: u8) {
+        self.code = code;
+    }
+
+    pub fn set_cksum(&mut self, cksum: u16) {
+        self.cksum = cksum;
+    }
+
+    pub fn set_options(&mut self, options: ICMPHeaderOptions) {
+        self.options = options;
+    }
+
+    pub fn get_type(&self) -> u8 {
+        match options {
+            ICMPHeaderOptions::Type0 { id, seqno } => 0,
+            ICMPHeaderOptions::Type3 { _unused, next_mtu } => 3,
+        };
+    }
+
+    pub fn get_code(&self) -> u8 {
+        self.code
+    }
+
+    pub fn get_cksum(&self) -> u16 {
+        self.cksum
+    }
+
+    pub fn get_options(&self) -> ICMPHeaderOptions {
+        self.options
+    }
+
+    pub fn encode(&self, buf: &mut [u8], offset: usize) -> SResult<usize> {
+        let mut off = offset;  
+        off = enc_consume!(buf, off; encode_u8, self.type_num);
+        off = enc_consume!(buf, off; encode_u8, self.code);
+        off = enc_consume!(buf, off; encode_u16, self.cksum);
+        off = enc_consume!(buf, off; encode_u32, self.options);
+        stream_done!(off, off);
+    }
+
+    pub fn decode(buf: &[u8]) -> SResult<ICMPHeader> {
+        // TODO: finish
+        let mut icmp_header = Self::new();
+        let off = 0;
+        stream_done!(off, icmp_header);
+    }
 }
 
-impl<'a> ICMPPacket<'a> {
-    pub fn reset(&self){}
-    pub fn get_offset(&self) -> usize{8}
+pub struct ICMPSendStruct<'a> {
+    ip_send_struct: &'a IP6SendStruct<'a>,
+    client: Cell<Option<&'a ICMPSendClient>>,
 }
