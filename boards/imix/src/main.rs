5dc5ecf9ee7aa9079617807331f346abde1242e0
Paul Crews <ptcrews@stanford.edu>
2018-04-24 14:59:59 -0700

    Renamed the test files inside boards/imix/src to be more descriptive.

--- boards/imix/src/main.rs
+++ boards/imix/src/main.rs
@@ -42,11 +42,11 @@ mod i2c_dummy;
 #[allow(dead_code)]
 mod spi_dummy;
 #[allow(dead_code)]
-mod lowpan_frag_dummy;
+mod ipv6_lowpan_test;
 #[allow(dead_code)]
-mod app_layer_lowpan_frag;
+mod udp_lowpan_test;
 #[allow(dead_code)]
-mod app_layer_icmp_lowpan_frag;
+mod icmp_lowpan_test;
 
 #[allow(dead_code)]
 mod aes_test;
